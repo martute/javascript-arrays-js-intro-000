c48fa1b174465757006ff56a220c25171132a862
Mario Tutek <m.tutek@gmail.com>
2021-04-05 13:18:11 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,8 @@
 var chocolateBars = ["snicker", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(chocolateBars, foo) {
-chocolateBars1 = ["foo", ...chocolateBars];
-return chocolateBars1;
+  chocolateBars1 = ["foo", ...chocolateBars];
+  return chocolateBars1;
 }
 
 function destructivelyAddElementToBeginningOfArray(chocolateBars, mueslli) {
