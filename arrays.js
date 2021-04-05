8eb843b2ec9e7876c2993f54bdffa7c8f6b78eb7
Mario Tutek <m.tutek@gmail.com>
2021-04-05 13:55:14 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -19,3 +19,7 @@ function destructivelyAddElementToEndOfArray(chocolateBars, foo) {
   chocolateBars.push("foo");
   return chocolateBars;
 }
+
+function accessElementInArray(chocolateBars, 2) {
+  return chocolateBars[2];
+}
