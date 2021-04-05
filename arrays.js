ce169ed77551f289aec987cf517151bc2147f498
Mario Tutek <m.tutek@gmail.com>
2021-04-05 15:56:38 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -40,6 +40,6 @@ function destructivelyRemoveElementFromEndOfArray(chocolateBars) {
 }
 
 function removeElementFromEndOfArray(chocolateBars) {
-  chocolateBars4 = chocolateBars.slice(3);
+  chocolateBars4 = chocolateBars.slice(0,2);
   return chocolateBars4;
 }
